dd60a2b9f4ae0f9fcaa65f97065bed59a876d581
Merve Silk <merve.silk@gmail.com>
2015-12-11 16:09:35 +0000

    Finishes Grandma programme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # bejeweled
+
+Various exercises in Ruby to refresh/ remember/ and just play with :D
+
+With some folders/ files made and designed just to improve on my command line skills
--- learn_to_programme/grandma3.rb
+++ learn_to_programme/grandma3.rb
@@ -3,20 +3,17 @@ bye_count = 0
 
 while true
   greeting = gets.chomp
-
-
-  if greeting == "BYE"
-    bye_count += 1
-  else
-    bye_count == bye_count
-  end
-  break if bye_count == 3
-  end
-
-  if egrandmas_reply = if greeting != greeting.upcase
-    puts "SPEAK UP CHILD!!!! YOURE WHISPERING AGAIN!!!"
-  else
-    puts "NOT SINCE #{rand(1929..1950)}"
+  break if bye_count == 2
+    if greeting == "BYE"
+      bye_count += 1
+    else
+      bye_count == bye_count
+    end
+    if grandmas_reply = if greeting != greeting.upcase
+      puts "SPEAK UP CHILD!!!! YOURE WHISPERING AGAIN!!!"
+    else
+      puts "NOT SINCE #{rand(1929..1950)}"
+    end
   end
 end
 
